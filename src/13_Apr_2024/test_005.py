00d1cebd3f24969638854f409801c60a2429caaf
Kalyani Potu <kalyani.potu@gmail.com>
2024-04-16 20:54:21 +0530

    added README.md

--- src/13_Apr_2024/test_005.py
+++ src/13_Apr_2024/test_005.py
@@ -3,6 +3,11 @@ from selenium import webdriver
 
 def test_open_vwologin():
     driver = webdriver.Chrome() # POST request | Create the Session
+    # Session is created - Unique ID - 16 digit ID
+    # 64avbdas6a6d89as9a7 - Session?
+    # webdriver.Chomre() - fresh copy of browser is created
+    # opens new tabs, opens url, those will be different from the normal browser.
+
     driver.get("https://app.vwo.com") # GET Request to URL param
     driver.maximize_window()
     print(driver.session_id)
